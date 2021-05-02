aeaac27a28cb62bc08e5360b05debfe4f9b5023a
Adam Kaluza <adam.kaluza.97@gmail.com>
2021-05-02 18:23:51 +0200

    lab7

---
+++ buffer.js
@@ -0,0 +1,17 @@
+function createBuffer() {
+
+    return (function (){
+        let z = "";
+        return function (name) {
+            if(name !== undefined){
+                z += name;
+            }
+            return z;
+        }
+    })();
+}
+let buffer = createBuffer();
+buffer('Data');
+buffer(' aequatione ');
+buffer('quotcunque');
+console.log(buffer());
